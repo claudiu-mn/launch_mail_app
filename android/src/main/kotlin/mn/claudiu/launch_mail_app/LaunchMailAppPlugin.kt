f26357b1b4c99f2264534249f54da207fa238af1
Claudiu <shout@claudiu.mn>
2021-08-11 15:00:46 +0300

    Fix crash on SM G960F

--- android/src/main/kotlin/mn/claudiu/launch_mail_app/LaunchMailAppPlugin.kt
+++ android/src/main/kotlin/mn/claudiu/launch_mail_app/LaunchMailAppPlugin.kt
@@ -1,11 +1,14 @@
 package mn.claudiu.launch_mail_app
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.LabeledIntent
 import android.net.Uri
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
@@ -13,14 +16,14 @@ import io.flutter.plugin.common.MethodChannel.Result
 
 
 /** LaunchMailAppPlugin */
-class LaunchMailAppPlugin: FlutterPlugin, MethodCallHandler {
+class LaunchMailAppPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private lateinit var channel : MethodChannel
   private lateinit var context: Context
-
+  private var activity: Activity? = null
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "launch_mail_app")
@@ -35,7 +38,7 @@ class LaunchMailAppPlugin: FlutterPlugin, MethodCallHandler {
         result.success(intent != null)
       }
       "launchMailApp" -> {
-        context.startActivity(intent)
+        activity?.startActivity(intent)
         result.success(null)
       }
       else -> {
@@ -79,5 +82,21 @@ class LaunchMailAppPlugin: FlutterPlugin, MethodCallHandler {
     } else {
       return null
     }
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
   }
 }
